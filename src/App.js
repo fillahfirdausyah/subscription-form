bd7601338423a0a94c83ffa194a51b737e149a21
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-06-08 16:11:30 +0700

    add screen validation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import PreviewPage from "./pages/PreviewPage";
 import MainPage from "./pages/MainPage";
+import PhonePage from "./pages/PhonePage";
 import { Switch, Route } from "react-router-dom";
 
-const ref = React.createRef();
-
 function App() {
+  const [layar, setLayar] = useState(window.innerWidth);
+
+  const updateLayar = () => {
+    setLayar(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateLayar);
+    return () => {
+      window.addEventListener("resize", updateLayar);
+    };
+  }, []);
+
   return (
     <div>
-      <Switch>
-        <Route path="/preview">
-          <PreviewPage />
-        </Route>
-        <Route path="/">
-          <MainPage />
-        </Route>
-      </Switch>
+      {layar > 768 ? (
+        <Switch>
+          <Route path="/preview">
+            <PreviewPage />
+          </Route>
+          <Route path="/">
+            <MainPage />
+          </Route>
+        </Switch>
+      ) : (
+        <PhonePage />
+      )}
     </div>
   );
 }
---
+++ src/pages/PhonePage/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css'
+
+function PhonePage() {
+  return (
+    <div className="container">
+      <div class="card text-white bg-dark-custom mb-3 card-custom phone-card text-center">
+        <div class="card-header">Informasi</div>
+        <div class="card-body">
+            <p className="card-text">
+                Anda Membuka di Layar Handphone silahkan buka dilayar Laptop atau Komputer
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PhonePage;
